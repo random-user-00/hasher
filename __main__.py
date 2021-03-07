28b861753c986f1b9c26fe1bbcff3f307c0eac8b
random-user-00 <34000489+random-user-00@users.noreply.github.com>
2021-03-07 20:00:52 +0100

    Added project leve __main__.py file

---
+++ __main__.py
@@ -0,0 +1,7 @@
+# This is just workaround so that users can run python -m hasher
+# instead of python -m hasher.hasher from directory of cloned git repository
+
+from .hasher import commandline
+
+if __name__ == "__main__":
+    commandline.execute_command()
