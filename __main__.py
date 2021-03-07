afa3984c63f33ebc9a3a5cad5c83ce4a11d91353
random-user-00 <34000489+random-user-00@users.noreply.github.com>
2021-03-07 20:03:13 +0100

    Corrected comments

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
-# This is just workaround so that users can run python -m hasher
-# instead of python -m hasher.hasher from directory of cloned git repository
+# This is just workaround so that users can run `python -m hasher`
+# instead of `python -m hasher.hasher` from directory of cloned git repository
 
 from .hasher import commandline
 
