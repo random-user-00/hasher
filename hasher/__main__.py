05c04ffcdc5c839aa31a9dd553218b529a642009
randomuser <34000489+random-user-00@users.noreply.github.com>
2021-03-14 13:47:22 +0100

    Added .flake8

--- hasher/__main__.py
+++ hasher/__main__.py
@@ -2,4 +2,3 @@ from . import commandline
 
 if __name__ == "__main__":
     commandline.execute_command()
-    
--- hasher/shared.py
+++ hasher/shared.py
@@ -2,9 +2,9 @@
 This module contains all the shared code, constants for hasher package.
 """
 import hashlib
+
 from . import __version__
 
 SUPPORTED_HASH = hashlib.algorithms_available
 PROG_NAME = "hasher"
 VERSION = __version__
-
