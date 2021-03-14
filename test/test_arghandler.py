fdae6909eeab9be93dfb867507476ea62f0e55c3
random-user-00 <34000489+random-user-00@users.noreply.github.com>
2021-03-14 13:16:16 +0100

    Corrected tests as they were failing on Linux platform

--- test/test_arghandler.py
+++ test/test_arghandler.py
@@ -7,13 +7,13 @@ class ParserTest(unittest.TestCase):
         return super().setUp()
 
     def test_help(self):
-        self.result = subprocess.check_output(["python", "-m", "hasher", "-h"], text=True, encoding="utf-8", shell=True, cwd="../")
-        self.assertIn("Description:", self.result)
-        self.assertIn("Required Arguments", self.result)
-        self.assertIn("ptional Arguments", self.result)
+        self.result = subprocess.run(["python", "-m", "hasher", "-h"], text=True, encoding="utf-8", cwd="../", capture_output=True)
+        self.assertIn("Description:", self.result.stdout)
+        self.assertIn("Required Arguments", self.result.stdout)
+        self.assertIn("ptional Arguments", self.result.stdout)
 
     def test_no_arg(self):
-        self.result = subprocess.run(["python", "-m", "hasher"], capture_output=True, text=True, encoding="utf-8", shell=True, cwd="../", check=False)
+        self.result = subprocess.run(["python", "-m", "hasher"], capture_output=True, text=True, encoding="utf-8", cwd="../", check=False)
         self.assertIn("usage:", self.result.stderr)
         self.assertIn("usage example:", self.result.stderr)
         self.assertIn("error:", self.result.stderr)
